9c17d44c9700e8c90c32aaa8c8729bbbbc0c9761
Hien Nguyen Van <hien.nguyen@titancorp.vn>
2017-12-21 15:01:44 +0700

    Create structure

---
+++ src/app/music/component/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import DefaultLayout from '../../../layout/default';
+import AppRoute from '../../../common/appRoute'
+
+let Data = {
+    Component: {
+
+    },
+    Router: {
+
+    }
+}
+
+let Index = [
+    
+]
+
+export default Index;
--- src/index.js
+++ src/index.js
@@ -4,6 +4,13 @@ import {BrowserRouter as Router, Switch} from 'react-router-dom'
 import registerServiceWorker from './registerServiceWorker';
 import Api from './app/api/component/';
 import Home from './app/home/component';
+import Article from './app/article/component';
+import Book from './app/book/component';
+import BookMp3 from './app/bookMp3/component';
+import Dictionary from './app/dictionary/component';
+import Music from './app/music/component';
+import Pagoda from './app/pagoda/component';
+import User from './app/user/component';
 import Error from './app/error/component';
 
 class App extends React.Component {
@@ -21,7 +28,41 @@ class App extends React.Component {
                             item
                         )
                     })}
-
+                    {Article.map((item) => {
+                        return (
+                            item
+                        )
+                    })}
+                    {Book.map((item) => {
+                        return (
+                            item
+                        )
+                    })}
+                    {BookMp3.map((item) => {
+                        return (
+                            item
+                        )
+                    })}
+                    {Dictionary.map((item) => {
+                        return (
+                            item
+                        )
+                    })}
+                    {Music.map((item) => {
+                        return (
+                            item
+                        )
+                    })}
+                    {Pagoda.map((item) => {
+                        return (
+                            item
+                        )
+                    })}
+                    {User.map((item) => {
+                        return (
+                            item
+                        )
+                    })}
 
                     {Error.map((item) => {
                         return (
